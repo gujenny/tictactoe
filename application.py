d53b37c8c2df1fb624a9b3cabe20ae85cc7b2e50
jenny800 <43099627+jenny800@users.noreply.github.com>
2019-01-21 00:41:48 -0500

    add tictactoe for Heroku

---
+++ application.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    if "board" not in session:
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["turn"] = "X"
+        session["moves"] = 0
+    over = isOver(session["board"], session["moves"])
+    return render_template("game.html", game=session["board"], turn=session["turn"], over=over[0], winner=over[1])
+
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+    session["moves"] += 1
+    session["board"][row][col] = session["turn"]
+    if session["turn"] == "X":
+        session["turn"] = "O"
+    else:
+        session["turn"] = "X"
+    return redirect(url_for("index"))
+
+@app.route("/newgame")
+def newgame():
+    session.clear()
+    return redirect("/")
+
+@app.route("/computer")
+def computerTurn():
+    move = minmax(session["board"], session["turn"])
+    return redirect("/play/" + str(move[1][0]) + "/" + str(move[1][1]))
+
+def isOver(board, moves):
+    for row in board:
+        if row[0] is not None and row[0] == row[1] == row[2]:
+            return (True, row[0])
+    for col in range(3):
+        if board[0][col] is not None and board[0][col] == board[1][col] == board[2][col]:
+            return (True, board[0][col])
+    if board[0][0] is not None and board[0][0] == board[1][1] == board[2][2]:
+        return (True, board[0][0])
+    if board[0][2] is not None and board[0][2] == board[1][1] == board[2][0]:
+        return (True, board[1][1])
+    if moves == 9:
+        return (True, None)
+    return (False, None)
+
+def minmax(curBoard, turn):
+    # X wins = 1, O wins = -1, tie = 0
+    possMoves = []
+    for i in range(3):
+        for j in range(3):
+            if curBoard[i][j] == None:
+                possMoves.append((i,j))
+    over = isOver(curBoard, 9-len(possMoves))
+    if over[0]:
+        if over[1] == "X":
+            return (1, None)
+        elif over[1] == "O":
+            return (-1, None)
+        else:
+            return (0, None)
+
+    if turn == "X":
+        maxmove = None
+        maxval = -2
+        for a in possMoves:
+            c = curBoard[:]
+            c[a[0]][a[1]] = "X"
+            result = minmax(c, "O")
+            c[a[0]][a[1]] = None
+            if result[0] > maxval:
+                maxval = result[0]
+                maxmove = a
+        return (maxval, maxmove)
+    else:
+        minmove = None
+        minval = 2
+        for a in possMoves:
+            c = curBoard[:]
+            c[a[0]][a[1]] = "O"
+            result = minmax(c, "X")
+            c[a[0]][a[1]] = None
+            if result[0] < minval:
+                minval = result[0]
+                minmove = a
+        return (minval, minmove)
